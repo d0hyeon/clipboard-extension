8fba6ad38b7906ff74dd86f11a643d1cfe0d225b
d0hyeon <ehgus6887@gmail.com>
2022-02-01 21:29:45 +0900

    chore: 바벨, 웹팩 html 플러그인 추가

--- config-override.js
+++ config-override.js
@@ -1,29 +1,54 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('react-scripts/node_modules/html-webpack-plugin')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  devServer(getConfigFunction) {
+    return ($1, $2) => {
+      const config = getConfigFunction($1, $2)
+      
+      return {
+        ...config,
+        devMiddleware: {
+          ...config.devMiddleware,
+          writeToDisk: true
+        }
+      }
+    }
+  },
   webpack (config, env) {
     let htmlPlugin = null;
-    const Plugins = config.plugins.filter((plugin) => {
-      if(plugin instanceof HtmlWebpackPlugin) {
-        htmlPlugin = plugin;
-        return false;
-      }
-      return true;
-    });
 
-    return {
+    const Plugins = env.mode === 'production' 
+      ? config.plugins.filter((plugin) => {
+          if(plugin instanceof HtmlWebpackPlugin) {
+            htmlPlugin = plugin;
+            return false;
+          }
+          return true;
+        }) 
+      : config.plugins
+
+    const nextConfig = {
       ...config,
       entry: {
-        main: config.entry,
-        background: path.resolve(__dirname, 'src/background.ts'),
-        content: path.resolve(__dirname, 'src/content.tsx')
+        main: path.resolve(__dirname, 'src/app/index.tsx'),
+        background: path.resolve(__dirname, 'src/background/index.tsx'),
+        content: path.resolve(__dirname, 'src/content/index.tsx')
       },
       output: {
         ...config.output,
         filename: `static/js/[name].js`,
         chunkFilename: `static/js/[name].js`,
+      },
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '~background': path.resolve(__dirname, 'src/background'),
+          '~content': path.resolve(__dirname, 'src/content'),
+          '~app': path.resolve(__dirname, 'src/app'),
+          '~common': path.resolve(__dirname, 'src/common'),
+        },
       },
       plugins: [
         ...(htmlPlugin 
@@ -35,5 +60,7 @@ module.exports = {
         ...Plugins
       ]
     }
+    return nextConfig
+    
   }
 }
