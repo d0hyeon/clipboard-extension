608af272d8fa8ee4990e8e79a27cdeb1aa18d5ce
d0hyeon <ehgus6887@gmail.com>
2022-02-02 02:38:52 +0900

    feat: 백그라운드 어플리케이션 추상화 및 로직 작성

--- src/background/index.tsx
+++ src/background/index.tsx
@@ -1,1 +1,40 @@
-export const title = 'background'
+class BackgroundApplication {
+  public currentActiveTabId: number | null = null
+
+  constructor () {
+    this.initialize()
+  }
+
+  public sendMessageOfTabState(tabId: number) {
+    if(this.currentActiveTabId) {
+      chrome.tabs.sendMessage(this.currentActiveTabId, { active: false }) 
+    }
+    chrome.tabs.sendMessage(tabId, { active: true })
+    this.currentActiveTabId = tabId
+  }
+
+  public initialize () {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+      const [currentTab] = tabs
+      console.log('tabs', tabs)
+      if(currentTab.id) {
+        this.sendMessageOfTabState(currentTab.id)
+      }
+    })
+    this.registeEventListeners()
+  }
+
+  public registeEventListeners() {
+    chrome.tabs.onActivated.addListener((activeInfo) => {
+      this.sendMessageOfTabState(activeInfo.tabId)
+    })
+  }
+}
+
+
+
+chrome.runtime.onInstalled.addListener(() => {
+  const backgroundApp =  new BackgroundApplication()
+  // @ts-ignore
+  globalThis.backgroundApp = backgroundApp
+});
