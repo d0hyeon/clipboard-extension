e9c4af217d1ccf6b57522b01531b5cf8e85a5316
d0hyeon <ehgus6887@gmail.com>
2022-02-02 02:37:03 +0900

    feat: 복사 후 크롬 스토리지에 저장까지의 로직 추가

--- src/content/App.tsx
+++ src/content/App.tsx
@@ -1,7 +1,70 @@
-import React from 'react';
-
+import { nanoid } from 'nanoid';
+import React, { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { clipboardAtom } from '~common/lib/atoms';
+import { CLIPBOARD_STORAGE_KEY } from '~common/lib/constants';
+import { getSelectionText } from '~common/lib/getSelectionText';
+import { ClipboardData } from '~common/lib/types';
 
 function App() {
+  const [clipboardState, setClipboardState] = useRecoilState(clipboardAtom)
+
+  useEffect(function initializeChromeStorage () {
+    chrome.storage.sync.get(CLIPBOARD_STORAGE_KEY, (dataMap) => {
+      const data = dataMap[CLIPBOARD_STORAGE_KEY]
+      if(data && data instanceof Array) {
+        setClipboardState(data)
+      } else {
+        chrome.storage.sync.set({
+          [CLIPBOARD_STORAGE_KEY]: []
+        })
+      }
+    })
+  }, [setClipboardState])
+
+  useEffect(function subscribeChromeStorageChange() {
+    chrome.storage.onChanged.addListener((dataMap) => {
+      const data = dataMap[CLIPBOARD_STORAGE_KEY]
+      if(data.newValue) {
+        setClipboardState(data.newValue)
+      }
+    })
+  }, [setClipboardState])
+
+  useEffect(function registeHandlerToCopyEvent() {
+    const handler = () => {
+      const selection = document.getSelection()
+      if(selection) {
+        const id = nanoid()
+        const text = getSelectionText(selection)
+        const clipboardData: ClipboardData = {
+          id,
+          text,
+          meta: {
+            url: window.location.href,
+            title: document.title
+          }
+        }
+        chrome.storage.sync.get(CLIPBOARD_STORAGE_KEY, (dataMap) => {
+          const data = dataMap[CLIPBOARD_STORAGE_KEY]
+          if(data instanceof Array) {
+            chrome.storage.sync.set({
+              [CLIPBOARD_STORAGE_KEY]: [
+                clipboardData,
+                ...data
+              ]
+            })
+          }
+        })
+      }
+    }
+
+    document.addEventListener('copy', handler)
+    return () => {
+      document.removeEventListener('copy', handler)
+    }
+  }, [setClipboardState])
+
   return (
     <>
       <div />
